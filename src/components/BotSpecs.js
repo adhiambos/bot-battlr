bfdeefe1be88425bd2d211240cd3004a17462d00
sharon adhiambo <adhiambosharonodhiambo@gmail.com>
2024-05-06 21:10:33 +0300

    upload bot specs

---
+++ src/components/BotSpecs.js
@@ -0,0 +1,13 @@
+// src/components/BotSpecs.js
+import React from 'react';
+
+const BotSpecs = ({ bot }) => {
+  return (
+    <div>
+      <h2>{bot.name}</h2>
+      {/* Display bot details */}
+    </div>
+  );
+};
+
+export default BotSpecs;
