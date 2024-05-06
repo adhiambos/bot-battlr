4b334a9f3b666cf4b80cd2c88b3f21c0ad51b41e
sharon adhiambo <adhiambosharonodhiambo@gmail.com>
2024-05-06 21:40:43 +0300

    update specs

--- src/components/BotSpecs.js
+++ src/components/BotSpecs.js
@@ -1,13 +1,24 @@
-// src/components/BotSpecs.js
-import React from 'react';
+import React from 'react'
 
-const BotSpecs = ({ bot }) => {
+function BotSpecs({ botselected, goBack, enlistBot}) {
+  const {id, avatar_url, name, catchphrase, bot_class, health, damage, armour, created_at, updated_at} = botselected;
+  const handleEnlist = () => {
+    enlistBot(botselected);
+    goBack();
+  }
   return (
-    <div>
-      <h2>{bot.name}</h2>
-      {/* Display bot details */}
+    <div className='ml-[40%]'>
+      <img src={avatar_url} alt={`Bot`} />
+      <h2 className='font-bold text-lg'>Name: {name}</h2>
+      <p>Catchphrase: {catchphrase}</p>
+      <h3 className='font-medium'>Class: {bot_class}</h3>
+      <p className='font-medium'>Health: {health} Damage: {damage} Armour: {armour}</p>
+      <p>Created at: {created_at}</p>
+      <p className='pb-4'>Updated at: {updated_at}</p>
+      <button className="bg-black text-white w-24 rounded-md" onClick={goBack}>Go back</button>
+      <button className="bg-black text-white w-24 rounded-md ml-4" onClick={handleEnlist}>Enlist</button>
     </div>
-  );
-};
+  )
+}
 
-export default BotSpecs;
+export default BotSpecs
