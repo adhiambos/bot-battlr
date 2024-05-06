aa6482a38f79c419c018d5b3bce8d8ed49a9ca0a
sharon adhiambo <adhiambosharonodhiambo@gmail.com>
2024-05-06 21:42:08 +0300

    update bot army

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,17 +1,26 @@
-// src/components/YourBotArmy.js
-import React from 'react';
+import React from 'react'
 
-const YourBotArmy = ({ bots }) => {
+function YourBotArmy({enlistedBots, releaseBot, dischargeBot }) {
+  // if (!Array.isArray(enlistedBots) || enlistedBots.length === 0) {
+  //   return <div className='bg-purple-500'>Your Bot Army is empty</div>;
+  // }
   return (
-    <div>
-      {bots.map(bot => (
-        <div key={bot.id}>
-          <h3>{bot.name}</h3>
-          {/* Display bot details */}
+    <div className='bg-purple-500 flex flex-wrap flex-row'>
+      <h2 className='text-4xl'>Your Bot Army</h2>
+      {enlistedBots.map((bot) => (
+        <div key={bot.id} className='p-[10px]' onClick={() => releaseBot(bot)}>
+          <img src={bot.avatar_url} alt="Bot" />
+          <h2 className='font-bold'>Name: {bot.name}</h2>
+          <p>Catchphrase: {bot.catchphrase}</p>
+          <h3>Class: {bot.bot_class}</h3>
+          <p className='font-medium'>
+            Health: {bot.health} Damage: {bot.damage}
+          </p>
+          <button className='bg-red-600 w-24 rounded-md' onClick={() => dischargeBot(bot)}>X</button>
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default YourBotArmy;
+export default YourBotArmy
